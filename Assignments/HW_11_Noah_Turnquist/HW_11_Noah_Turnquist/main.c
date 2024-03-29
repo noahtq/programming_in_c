d06ef5108a037734c9f8a3b654ec36143e53a048
noahtq <vfx.noaht@gmail.com>
2024-03-29 09:45:47 -0500

    debugging and optimizing

--- Assignments/HW_11_Noah_Turnquist/HW_11_Noah_Turnquist/main.c
+++ Assignments/HW_11_Noah_Turnquist/HW_11_Noah_Turnquist/main.c
@@ -1,11 +1,12 @@
 //HW #11, Noah Turnquist
 
 //TODO: CHECK TO MAKE SURE PROGRAM DOESN'T CONTAIN ANY STRCHR(), STRRCHR(), or MEMCHR() calls.
-//TODO: See if I should discard newline character at the end string when reading it in from fgets()
-//TODO: Fix warning about long to int
-//TODO: Make loops more efficient using pointer notation
-//TODO: Find a way to discard new line character in userString
-//TODO: Replace scanf for character with something more appropriate
+//TODO: Email Ken: ask about fpurge()
+//TODO: Email Ken: See if I should discard newline character at the end string when reading it in from fgets()
+//TODO: Email Ken: Had to use type long for location array to get rid of some compiler warning. Is this okay?
+//TODO: Make loops more efficient using pointer notation?
+//TODO: Keep or discard newline character in userString?
+//TODO: Break up program into more discrete functions for better readability and maintanability
 
 #include <stdio.h>
 #include <string.h>
@@ -14,23 +15,30 @@
 #define MAXMATCHES 10
 #define FLUSH while(getchar() != '\n')
 
-void ClearBuffer(void);
-int CharIsAt(char* pStr, char ch, int loc[], int mLoc);
+int CharIsAt(char* pStr, char ch, long loc[], int mLoc);
 int FMin(int x, int y);
 
 int main(void) {
     char userString[SIZE];
-    int locations[MAXMATCHES];
+    long locations[MAXMATCHES];
     char searchCh;
     int matches;
     
-    //TODO: Fix issue of input buffer not being flushed as necessary
     printf("HW #11, Noah Turnquist\n");
-    printf("Enter string to be searched. (Blank line to quit):\n");
-    while (*fgets(userString, SIZE - 1, stdin) != '\n') {
-        ClearBuffer();
+    printf("Enter string to be searched. (Blank line to quit): ");
+    while (*fgets(userString, SIZE, stdin) != '\n') {
+        fpurge(stdin); //TODO: Ask prof is this is an okay way to clear the buffer
+        
+        //Forcing the second to last element in string to be a newline...
+        //If the user enters more than SIZE - 1 characters.
+        //Ending of string will look like [..., \n, \0]
+        if (strlen(userString) >= SIZE - 1) {
+            userString[SIZE - 2] = '\n';
+        }
+        
         printf("Enter character to search for: ");
-        scanf(" %c", &searchCh);
+        searchCh = getc(stdin);
+        FLUSH;
         
         matches = CharIsAt(userString, searchCh, locations, MAXMATCHES);
         
@@ -39,36 +47,37 @@ int main(void) {
         int matchesToPrint = FMin(matches, MAXMATCHES);
         printf("The first %d matches are at: ", matchesToPrint);
         for (int i = 0; i < matchesToPrint; i++) {
-            printf("%2d ", locations[i]);
+            printf("%2ld ", locations[i]);
         }
         printf("in the string:\n");
         printf("%s", userString);
-        for (int i = 0; i < strlen(userString); i++) { //TODO: Could make slightly more efficient by terminating once we've reached matches
-            int match = 0;
-            for (int j = 0; j < matchesToPrint; j++) {
-                if (i == locations[j]) {
-                    putchar('^');
-                    match = 1;
-                }
-            }
-            if (match == 0) putchar(' ');
-        }
-        printf("\n\n");
         
-        printf("Please enter a string:\n");
-        FLUSH;
+        int lenUserString = (int) strlen(userString);
+        char underline[lenUserString + 1];
+        for (int i = 0; i < lenUserString; i++) {
+            underline[i] = ' ';
+        }
+        
+        for (int i = 0; i < matchesToPrint; i++) {
+            long matchIndex = locations[i];
+            underline[matchIndex] = '^';
+        }
+        
+        printf("%s\n\n", underline);
+        
+        printf("Enter string to be searched. (Blank line to quit): ");
     }
     
     return 0;
 }
 
-int CharIsAt(char* pStr, char ch, int loc[], int mLoc) {
+int CharIsAt(char* pStr, char ch, long loc[], int mLoc) {
     int matches = 0;
     
     for (char* itr = pStr; *itr != '\0'; itr++) {
         if (*itr == ch) {
             if (matches < mLoc) {
-                loc[matches] = itr - pStr;
+                loc[matches] = (long) ((long) itr - (long) pStr);
             }
             matches++;
         }
@@ -80,7 +89,20 @@ int FMin(int x, int y) {
     return x < y ? x : y;
 }
 
-void ClearBuffer(void) {
-    char ch;
-    while( (ch = getchar()) != '\n' && ch != EOF );
-}
+
+//VERIFICATION
+/*
+ 1. Check under normal conditions
+    String = "hello" Char = 'l'
+    String = "This is fun" Char = 'h'
+ 2. Check with no matching characters
+    String = "Green" Char = 'a'
+ 3. Check with more than MAXMATCHES characters
+    String = "aaaaaaaaaaaaaaaaaaaaaaaaaaa", char = 'a'
+ 4. Check with userString overflow
+    String = 'adadadadadadadadadadadadadadadadadadadadadadadadadadadadadadadadadadada', char = 'd'
+ 5. Check with ending program on first loop iteration.
+    String = "\n"
+ 6. Check with entering some numbers.
+    String = "12456678", Char = '6'
+ */
