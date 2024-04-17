ef22becc674dea011b68a3f23ca0f796d51767c6
noahtq <vfx.noaht@gmail.com>
2024-04-17 06:40:23 -0500

    Creating project for HW14

---
+++ Assignments/HW_14_Noah_Turnquist/HW_14_Noah_Turnquist/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  HW_14_Noah_Turnquist
+//
+//  Created by Noah Turnquist on 4/17/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
