0a2448c793f2708db8f2cff9eb710078dd7228df
noahtq <vfx.noaht@gmail.com>
2024-04-30 11:20:54 -0500

    Create HW16 Xcode proect

---
+++ Assignments/HW_16_Noah_Turnquist/HW_16_Noah_Turnquist/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  HW_16_Noah_Turnquist
+//
+//  Created by Noah Turnquist on 4/30/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
